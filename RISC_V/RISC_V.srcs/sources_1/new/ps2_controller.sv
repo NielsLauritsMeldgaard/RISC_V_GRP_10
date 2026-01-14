1f359616f053b581b13ed921c61a46c83b0e94ee
AugustDyrloevDTU <s234357@dtu.dk>
2026-01-14 15:24:06 +0100

    Add seven segment controller, I/O manager, PS2 controller, UART controller, and Wishbone interconnect modules

---
+++ RISC_V/RISC_V.srcs/sources_1/new/ps2_controller.sv
@@ -0,0 +1,11 @@
+module ps2_controller (
+    input  logic        clk, rst,
+    input  logic        ps2_clk,     // Physical Clock pin
+    input  logic        ps2_data,    // Physical Data pin
+    output logic [7:0]  key_code_o,  // Last scan code received
+    output logic        data_ready_o // New key pressed pulse
+);
+    // TODO: Implement 11-bit shift register and parity check
+    assign key_code_o   = 8'h1C;     // Constant 'A' key for testing
+    assign data_ready_o = 1'b0;
+endmodule

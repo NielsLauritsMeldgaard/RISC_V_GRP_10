0b17544ecedae75166624d52b6c96841682f374b
NielsLauritsMeldgaard <62181856+NielsLauritsMeldgaard@users.noreply.github.com>
2026-01-16 09:53:58 +0100

    added a simple testbench that just asserts reset and run for easy waveform generation

---
+++ RISC_V/RISC_V.srcs/sim_1/new/testbench_simple.sv
@@ -0,0 +1,25 @@
+  `timescale 1ns / 1ps
+
+ module tb_datapath_simple();
+
+    logic clk, rst;
+    
+    // Simple clock/reset
+    initial begin
+         clk = 0;
+         forever #5 clk = ~clk;
+    end
+    
+    initial begin
+        rst = 1'b1;      // assert reset
+        #10;             // hold reset
+        rst = 1'b0;      // deassert reset    
+    end
+    
+    // DUT
+    datapath #(.MEM_WORDS(128)) dut (
+        .clk(clk),
+        .rst(rst)
+    );
+     
+ endmodule

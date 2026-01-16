dfc8e897feccce4cdfe8fd0d4708bd95de52ab5e
NielsLauritsMeldgaard <62181856+NielsLauritsMeldgaard@users.noreply.github.com>
2026-01-16 09:53:19 +0100

    The seven segment display driver does not support the seperation point yet

--- RISC_V/RISC_V.srcs/sources_1/new/7seg_dec.sv
+++ RISC_V/RISC_V.srcs/sources_1/new/7seg_dec.sv
@@ -4,7 +4,7 @@ module seven_seg_controller (
     input  logic        clk, rst,
     input  logic [31:0] data_i,    // 32-bit data from CPU (e.g., 4x8-bit hex codes)
     input  logic        we,        // Write enable from io_manager
-    output logic [7:0]  segments,  // Physical pins: A, B, C, D, E, F, G, DP
+    output logic [6:0]  segments,  // Physical pins: A, B, C, D, E, F, G, DP
     output logic [3:0]  anodes     // Physical pins: Digit selector
 );
     // Placeholder for internal register
